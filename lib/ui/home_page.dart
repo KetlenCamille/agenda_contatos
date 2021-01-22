5189eafb146105ff0c21f7381f92d7dd931e0b1c
KetlenCamille <ketlen.rieper@gmail.com>
2021-01-21 22:15:27 -0300

    Creatinf BottomSheet with function Call

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:agenda_contatos/ui/contact_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -116,7 +117,8 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     onPressed:(){
-
+                      launch("tel:${contacts[index].phone}");
+                      Navigator.pop(context);
                     },
                   ),
                   ),
